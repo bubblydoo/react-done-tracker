1f3cb0dc1192458bf3a3bcf3370137f74a2d931b
Hans Otto Wirtz <hansottowirtz@gmail.com>
2022-12-28 14:32:37 +0100

    Use storybook vite

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
   },
-  "docs": {
-    "docsPage": true
-  },
+  docs: {
+    docsPage: true,
+    autodocs: true
+  }
 };
